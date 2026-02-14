cd183b4843f411829b95303ef242a6d76f7a7b7f
atullnmiit <68318318+atullnmiit@users.noreply.github.com>
2026-02-14 21:50:10 +0530

    Rename storage buckets and update project name

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "bkt-demo-000"
+  name          = "demo-bucket-1"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "Github-GCP-WIF-Test"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name          = "bkt-demo-002"
+  name          = "demo-bucket-2"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "Github-GCP-WIF-Test"
   force_destroy = true
   public_access_prevention = "enforced"
 }
